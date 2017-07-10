bc14d58175081f35c015b37907ac939a2c45d647
techmmr <mmishra@grepruby.com>
2017-07-10 17:46:31 +0530

    app shell completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
