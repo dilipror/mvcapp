38d4d338e4cbe3c9d5457b76a7cd16c4679caaf0
techmmr <mmishra@grepruby.com>
2017-07-10 14:40:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
