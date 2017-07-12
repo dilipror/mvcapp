6857b36ddcf3cf185f6105e1a670b8e7c9bdb5f3
techmmr <mmishra@grepruby.com>
2017-07-12 20:25:40 +0530

    change /home to /

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 import * as home from '../controllers/home';
 
-router.post('/home', home.addToCart);
-router.get('/home', home.renderHome);
 router.post('/', home.addToCart);
 router.get('/', home.renderHome);
 
